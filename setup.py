49e6a6206eab65a3ffd114c4d311954e9dab6b3f
Romain Touzé <romain.touze@gmail.com>
2012-12-28 19:00:05 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ setup (
         description='A classical Pong clone using pygame',
         install_requires=['pygame'],
         long_description=open('README.markdown').read(),
+        classifiers=[
+            "License :: WTFPL",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2.6",
+            "Topic :: Games",
+            ],
         packages=find_packages(),
         include_package_data=True,
         entry_points={
